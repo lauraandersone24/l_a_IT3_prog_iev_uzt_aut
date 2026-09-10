1fe6bb3a9bbb17f1c8d54ecb147bd7d1e7e3f188
Laura Andersone <lauraandersone@Lauras-MacBook-Air.local>
2026-09-10 17:34:30 +0300

    pievienot add_item, mark_done, get_all funkcijas

---
+++ main.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, MediaItem
+
+engine = create_engine("sqlite:///media.db")
+Session = sessionmaker(bind=engine)
+
+
+def add_item(session, title, type_, status="planned"):
+    item = MediaItem(title=title, type=type_, status=status)
+    session.add(item)
+    session.commit()
+    return item
+
+
+def mark_done(session, item_id):
+    item = session.get(MediaItem, item_id)
+    if item:
+        item.status = "done"
+        session.commit()
+    return item
+
+
+def get_all(session):
+    return session.query(MediaItem).all()
+
+
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
+    session = Session()
+    add_item(session, "Dune", "book")
+    for item in get_all(session):
+        print(item.title, item.type, item.status)
