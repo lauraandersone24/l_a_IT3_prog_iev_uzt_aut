ce113e63ceba7cfd3c0810d561322236826dcb3e
Laura Andersone <lauraandersone@Lauras-MacBook-Air.local>
2026-09-10 17:34:16 +0300

    pievienot MediaItem modeli

---
+++ models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class MediaItem(Base):
+    __tablename__ = "media_items"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    type = Column(String, nullable=False)    # "book" / "movie" / "music"
+    status = Column(String, default="planned")  # "planned" / "in_progress" / "done"
